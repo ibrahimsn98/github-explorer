76d253a71795cc76513badb71574cc572d46d7f4
ibrahimsn98 <ibrahimsn98@gmail.com>
2018-12-28 15:40:32 +0300

    fetch data on installed

--- js/loader.js
+++ js/loader.js
@@ -1,30 +1,36 @@
-$(document).ready(function() {
-	var date = new Date();
-	date.setDate(date.getDate() - 45);
-	var dateFilter = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();  
+var date = new Date();
+date.setDate(date.getDate() - 45);
+var dateFilter = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();  
 
-	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-		chrome.storage.sync.get({keyword: "", language: ""}, function(data) {
-			var url = "https://api.github.com/search/repositories?q="+data.keyword;
+chrome.runtime.onInstalled.addListener(function() {
+	fetch();
+});
 
-			if (data.language != "")
-				url += "+language:" + data.language;
-			
-			url += "+stars:>=10+created:>" + dateFilter + "&sort=stars&order=desc&per_page=200";
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	fetch();
+});
 
-			$.get(url, function(data) {
-				var add = [];
+var fetch = function() {
+	chrome.storage.sync.get({keyword: "", language: ""}, function(data) {
+		var url = "https://api.github.com/search/repositories?q="+data.keyword;
 
-				data.items.forEach(function(item) {
-					if (item.description != null && item.language != null)
-						add.push({name: item.name, url: item.html_url, owner: item.owner.login,
-							description: item.description,
-						  	language: item.language, stars: item.stargazers_count,
-						  	issues: item.open_issues_count})
-				});
-					
-				chrome.storage.local.set({"data": add});
+		if (data.language != "")
+			url += "+language:" + data.language;
+		
+		url += "+stars:>=10+created:>" + dateFilter + "&sort=stars&order=desc&per_page=125";
+
+		$.get(url, function(data) {
+			var add = [];
+
+			data.items.forEach(function(item) {
+				if (item.description != null && item.language != null)
+					add.push({name: item.name, url: item.html_url, owner: item.owner.login,
+						description: item.description,
+					  	language: item.language, stars: item.stargazers_count,
+					  	issues: item.open_issues_count})
 			});
+				
+			chrome.storage.local.set({"data": add});
 		});
 	});
-});
+};
